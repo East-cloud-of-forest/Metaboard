44a8ce2ad56be794141e7a07c14781d2903b1078
dongun lim <ecof1911@gmail.com>
2022-04-01 19:45:49 +0900

    메모추가시 가운데에 화면중앙에 생성되도록 수정
    async를 이용하여 v-for이 추가 된 이후 div 정보를 받아옴
    함수내에서 this를 사용할수 없음 인자로 전달

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ new Vue({
     memotitle: '',
     memocontent: '',
     memolist: [],
+    id: 0,
     moveon: false,
     targetdiv: '',
     targetshiftx: 0,
     targetshifty: 0,
-    targetlist : 0,
-    targetsize : {left : 0, top : 0}
+    targetlist: 0,
+    targetsize: { left: 50, top: 50 },
   },
   methods: {
     // 휠 감지
@@ -25,7 +26,6 @@ new Vue({
         console.log('Up' + e.deltaY)
       }
     },
-
 
     // 카드 드래그 엔 드랍 이벤트
     // 참고 https://ko.javascript.info/mouse-drag-and-drop
@@ -76,24 +76,51 @@ new Vue({
       }
     },
     addMemo() {
+      // 유효성 검사
       const validate = this.$refs.form.validate()
       if (validate) {
-        this.memolist.push({
-          title: this.memotitle,
-          content: this.memocontent,
-          left : 0,
-          top : 0,
+        creatememo(
+          this.memolist,
+          this.memotitle,
+          this.memocontent,
+          this.id,
+          this.formopen,
+          // 리스트 추가 후 발동
+        ).then(() => {
+          let t = document.getElementById(this.id)
+          t.style.left = `calc(50% - ${t.getBoundingClientRect().width / 2}px)`
+          t.style.top = `calc(50% - ${t.getBoundingClientRect().height / 2}px)`
+
+          this.id++
+          this.formopen = false
+          this.memotitle = ''
+          this.memocontent = ''
         })
-        this.formopen = false
-        this.memotitle = ''
-        this.memocontent = ''
+      }
+
+      // 메모 리스트에 추가 함수
+      async function creatememo(
+        memolist,
+        memotitle,
+        memocontent,
+        id,
+      ) {
+        memolist.push({
+          title: memotitle,
+          content: memocontent,
+          left: 0,
+          top: 0,
+          id: id,
+        })
       }
     },
   },
 
   // 마우스 벗어났을때 이벤트 중지 방지
   mounted() {
-    window.addEventListener('mouseup', this.onCardUp)
-    window.addEventListener('mousemove', this.onCardMove)
+    this.$nextTick(function () {
+      window.addEventListener('mouseup', this.onCardUp)
+      window.addEventListener('mousemove', this.onCardMove)
+    })
   },
 })
